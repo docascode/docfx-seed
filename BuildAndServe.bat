dd7d56e14f20d0c067c831460e222ef04504d2e7
peterennis <peterennis@yahoo.com>
2016-07-19 21:08:25 -0700

    gitignore themes folder, update BuildAndServe.bat with examples of template export commands

--- BuildAndServe.bat
+++ BuildAndServe.bat
@@ -1,3 +1,16 @@
-.\docfx\docfx.exe docfx.json --serve
+%theme%=ae
+
+.\docfx\docfx.exe template list --help
+.\docfx\docfx.exe template list --all
+REM This will export the default theme
+REM .\docfx\docfx.exe template export default --output "C:\ae\docfx-seed\themes\default"
+REM This will export all themes
+REM .\docfx\docfx.exe template export --all --output "C:\ae\docfx-seed\themes"
+
+
+goto exit:
+.\docfx\docfx.exe --serve
+
+:exit
 echo Press any key to exit
 pause > nul
