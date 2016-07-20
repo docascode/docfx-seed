9ab859258d78c49475fe67257ee2879699cd0694
peterennis <peterennis@yahoo.com>
2016-07-19 21:36:14 -0700

    update BuildAndServe.bat

--- BuildAndServe.bat
+++ BuildAndServe.bat
@@ -1,4 +1,4 @@
-%theme%=ae
+set ae="C:\ae\docfx-seed"
 
 .\docfx\docfx.exe template list --help
 .\docfx\docfx.exe template list --all
@@ -7,10 +7,12 @@ REM .\docfx\docfx.exe template export default --output "C:\ae\docfx-seed\themes\
 REM This will export all themes
 REM .\docfx\docfx.exe template export --all --output "C:\ae\docfx-seed\themes"
 
+copy %ae%\themes\ae\images\favicon.ico" %ae%\_site\favicon.ico"
+copy %ae%\themes\ae\images\logo.svg" %ae%\_site\logo.svg"
 
-goto exit:
-.\docfx\docfx.exe --serve
 
-:exit
+REM Serve the site
+.\docfx\docfx serve _site
+
 echo Press any key to exit
 pause > nul
