63bf8dcaac2075ca6705b55363382916690d2cdc
Liangying.Wei <lianwei@microsoft.com>
2018-03-23 10:38:47 +0800

    Add files via upload

---
+++ test/msbuildlocaterrepro.net461/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Build.Locator;
+
+namespace msbuildlocaterrepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MSBuildLocator.QueryVisualStudioInstances().Count());
+        }
+    }
+}
